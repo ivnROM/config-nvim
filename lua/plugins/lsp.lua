211c6ce2e8539f0843fe2d420144be762266bd0f
iván <meyerivanoliver@gmail.com>
2024-09-09 17:15:37 -0300

    cambio lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -126,7 +126,7 @@ local lsp_attach = function(client, bufnr)
       -- vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
   -- vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
   vim.keymap.set('n', '<leader>k', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.keymap.set('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 end
 
 lsp_zero.extend_lspconfig({
