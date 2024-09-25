da8d91fd7dd8175e2eb3c11c90bb4c9cce3f84d6
iván <meyerivanoliver@gmail.com>
2024-09-25 15:12:19 -0300

    cambios??

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,6 +1,6 @@
 function Colorify(color)
-	-- color = color or "gruber-darker"
-	color = color or "lavish-dark"
+	color = color or "gruber-darker"
+	-- color = color or "lavish-dark"
 	vim.cmd.colorscheme(color)
 end
 
